9164d37fb41c2a4ca25211adefc49a0b79b53e2d
student <student@vlab.local>
2023-11-09 12:54:13 +0100

    Initial commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int temp;
+
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+
+        System.out.print("Before: ");
+        printArray(array);
+
+        System.out.print("After: ");
+        printArray(array);
+    }
+
+    public static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+}
